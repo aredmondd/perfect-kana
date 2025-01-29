de71e71db1592062e85d4c858bec22228bd46d58
“Aiden <adnrdmnd@gmail.com>
2025-01-29 09:49:21 -0500

    add romaji below hiragana

--- src/components/KanaCard.vue
+++ src/components/KanaCard.vue
@@ -1,25 +1,26 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import hiraganaKey from '@/hiragana-array';
+
+const selectedHiragana = ref('');
+const selectedRomaji = ref('');
+
+const generateNewHiragana = () => {
+    const randomIndex = Math.floor(Math.random() * hiraganaKey.length);
+    selectedHiragana.value = hiraganaKey[randomIndex][0];
+    selectedRomaji.value = hiraganaKey[randomIndex][1];
+};
+
+// Initialize on mount
+onMounted(generateNewHiragana);
 </script>
 
 <template>
-    <div class="border-4 border-b-8 border-text rounded-3xl px-20 py-24 flex justify-center items-center">
-        <p class="font-NSJ text-9xl font-bold">{{ selectedHiragana }}</p>
+    <div class="border-4 border-b-8 border-text rounded-4xl px-20 py-20 flex justify-center items-center">
+        <div class="flex flex-col items-center justify-center">
+            <p class="font-NSJ text-9xl font-bold">{{ selectedHiragana }}</p>
+            <p class="text-md mt-4 opacity-50 tracking-wide">{{ selectedRomaji }}</p>
+        </div>
     </div>
     <button @click="generateNewHiragana" class="material-symbols-outlined border-2 rounded-md px-2 py-1 mt-6 hover:bg-accent transition-all duration-250 ease-in-out">shuffle</button>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            selectedHiragana: hiraganaKey[Math.floor(Math.random() * hiraganaKey.length)][0]
-        }
-    },
-    methods: {
-        generateNewHiragana() {
-            this.selectedHiragana = hiraganaKey[Math.floor(Math.random() * hiraganaKey.length)][0];
-        }
-    }
-}
-</script>
